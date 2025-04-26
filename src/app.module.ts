5b1cb99111a604c6da30cee82833f4372e5d905b
HugeKyung <kiki9510@gmail.com>
2025-04-26 15:04:46 +0900

    feat: controller, service, module 클래스 및 파일 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BoardModule } from './modules/board/board.module';
+import { CommentModule } from './modules/comment/comment.module';
+import { NotificationModule } from './modules/notification/notification.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [BoardModule, CommentModule, NotificationModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
